2411103b4d92c3165e034d0b8b5a51ee55ac202f
Christopher S. Case <chris.case@g33xnexus.com>
2016-07-14 11:58:02 -0500

    Added a tunnel script for working with the 'production' database locally.

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
         port: process.env.SERVER_PORT || 8080
     },
     rethink: {
-        host: process.env.RETHINK_DB_HOST || 'pinkie.skewedaspect.com',
+        host: process.env.RETHINK_DB_HOST || 'localhost',
         port: process.env.RETHINK_DB_PORT || 28015,
         db: 'pgo'
     }
